6025c99656f99449ca3dcd3b72c46b1d311cad31
apurva313 <apurvakumar313@gmail.com>
2024-05-26 03:24:00 +0530

    Create models file

---
+++ models/groceryModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const grocerySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const Grocery = mongoose.model('Grocery', grocerySchema);
+
+module.exports = Grocery;
